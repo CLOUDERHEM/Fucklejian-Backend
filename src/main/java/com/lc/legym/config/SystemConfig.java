980ed4bbe7cbf587b138f245a6bbcae8a85f3863
CLOUDERHEM <cloudac@qq.com>
2022-09-09 18:29:37 +0800

    perf: 修改线程池配置

--- src/main/java/com/lc/legym/config/SystemConfig.java
+++ src/main/java/com/lc/legym/config/SystemConfig.java
@@ -7,24 +7,12 @@ package com.lc.legym.config;
 public final class SystemConfig {
 
 
-    /**
-     * 运行结果保存的最大时长, 默认15s
-     */
     public static final int MAX_OBJECT_EXIST_TIME = 30 * 1000;
 
-    /**
-     * 核心线程数量
-     */
-    public static final int CORE_POOL_SIZE = 5;
+    public static final int CORE_POOL_SIZE = 8;
 
-    /**
-     * 应靠近cpu核心数
-     */
-    public static final int MAX_POOL_SIZE = 13;
+    public static final int MAX_POOL_SIZE = 8;
 
-    /**
-     * 提交的任务数目超过 QUEUE_CAPACITY + MAX_POOL_SIZE 时候, 任务将被丢弃
-     */
     public static final int QUEUE_CAPACITY = 100;
 
 
