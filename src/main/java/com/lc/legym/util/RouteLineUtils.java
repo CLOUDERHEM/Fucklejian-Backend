18277337f2abdeae79aacf285ff1328c94f43ab5
CLOUDERHEM <cloudac@qq.com>
2022-09-15 13:25:11 +0800

    fix: 跑步距离过大产生的标记点过多问题

--- src/main/java/com/lc/legym/util/RouteLineUtils.java
+++ src/main/java/com/lc/legym/util/RouteLineUtils.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Random;
 
 import static com.lc.legym.enums.Constant.DISTANCE_BETWEEN_POINT;
+import static com.lc.legym.enums.Constant.MAX_ROUTE_LINE_SIZE;
 
 /**
  * @author Aaron Yeung
@@ -48,7 +49,7 @@ public class RouteLineUtils {
         return getRouteLine(routeLine);
     }
 
-    public static List<LatLng> getRouteLine(List<LatLng> routeLine) {
+    public static List<LatLng> getRouteLine(List<LatLng> routeLine) throws Exception {
         List<LatLng> result = new ArrayList<>(250);
         for (int i = 1; i < routeLine.size(); i++) {
             LatLng start = routeLine.get(i - 1);
@@ -70,7 +71,7 @@ public class RouteLineUtils {
         ).getEllipsoidalDistance();
     }
 
-    public static void addRouteLine(List<LatLng> result, LatLng start, LatLng end, int pieces) {
+    public static void addRouteLine(List<LatLng> result, LatLng start, LatLng end, int pieces) throws Exception {
 
         double periodLat = (end.getLatitude() - start.getLatitude()) / pieces;
         double periodLng = (end.getLongitude() - start.getLongitude()) / pieces;
@@ -82,6 +83,11 @@ public class RouteLineUtils {
             result.add(latLng);
             lastLat = latLng.getLatitude();
             lastLng = latLng.getLongitude();
+
+            // 跑步点数过多乐健会异常
+            if (result.size() > MAX_ROUTE_LINE_SIZE) {
+                throw new Exception("跑步距离过大, 偏移后点数量过多, 跑步错误");
+            }
         }
     }
 
