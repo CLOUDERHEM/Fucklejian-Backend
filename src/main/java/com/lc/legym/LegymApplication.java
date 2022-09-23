09c4a68b26f64f6cfd304c7d80001b69aeaa24de
CLOUDERHEM <cloudac@qq.com>
2022-09-23 08:50:56 +0800

    fix: 时区错误

--- src/main/java/com/lc/legym/LegymApplication.java
+++ src/main/java/com/lc/legym/LegymApplication.java
@@ -3,6 +3,8 @@ package com.lc.legym;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 /**
  * @author Aaron Yeung
  */
@@ -10,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LegymApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(LegymApplication.class, args);
     }
 
