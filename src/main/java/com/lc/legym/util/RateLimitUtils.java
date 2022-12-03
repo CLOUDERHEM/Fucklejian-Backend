eb89b788630065946df92df1fd2e74f5504f6e05
CLOUDERHEM <cloudac@qq.com>
2022-12-03 20:03:33 +0800

    refactor: delete map

--- src/main/java/com/lc/legym/util/RateLimitUtils.java
+++ src/main/java/com/lc/legym/util/RateLimitUtils.java
@@ -2,9 +2,6 @@ package com.lc.legym.util;
 
 import com.google.common.util.concurrent.RateLimiter;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import static com.lc.legym.enums.Constant.PERMITS_PER_SECOND;
 
 /**
@@ -14,22 +11,11 @@ import static com.lc.legym.enums.Constant.PERMITS_PER_SECOND;
 @SuppressWarnings("all")
 public class RateLimitUtils {
 
-    private static final Map<String, RateLimiter> STRING_LIMITER_MAP = new ConcurrentHashMap<>();
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(PERMITS_PER_SECOND);
 
     public static boolean tryAcquire(String remoteAdd) {
-        RateLimiter limiter;
-        if (STRING_LIMITER_MAP.get(remoteAdd) == null) {
-            synchronized (STRING_LIMITER_MAP) {
-                if (STRING_LIMITER_MAP.get(remoteAdd) == null) {
-                    limiter = RateLimiter.create(PERMITS_PER_SECOND);
-                    STRING_LIMITER_MAP.put(remoteAdd, limiter);
-                    return true;
-                }
-            }
-        }
-
-        limiter = STRING_LIMITER_MAP.get(remoteAdd);
-        return limiter.tryAcquire();
+        // todo remoteAdd
+        return RATE_LIMITER.tryAcquire();
     }
 
 }
