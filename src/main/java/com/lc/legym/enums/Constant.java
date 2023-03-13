ee620133ec99aef6ef4486204a1699b1e8e768e5
CLOUDERHEM <cloudac@qq.com>
2023-03-13 17:28:50 +0800

    update app version 30000818

--- src/main/java/com/lc/legym/enums/Constant.java
+++ src/main/java/com/lc/legym/enums/Constant.java
@@ -8,6 +8,6 @@ public class Constant {
     public static final int MAX_MSG_LENGTH = 50;
     public static final int DISTANCE_BETWEEN_POINT = 4;
     public static final int MAX_ROUTE_LINE_SIZE = 1500;
-    public static final int APP_VERSION = 30000812;
+    public static final int APP_VERSION = 30000818;
     public static final double MAX_MILE = 3.0D;
 }
